f51b3832fd203af2dabc7d72f9874bbf4de065c9
sebastian <sebas61018@gmail.com>
2025-05-23 21:22:35 -0500

    cambios

--- onclick/astro.config.mjs
+++ onclick/astro.config.mjs
@@ -2,15 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify'; 
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  adapter: netlify(), 
   integrations: [mdx(), sitemap()],
-
   vite: {
-    plugins: [tailwindcss()],
-  },
+    plugins: [tailwindcss()]
+  }
 });
